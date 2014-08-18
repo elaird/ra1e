5f0ed99f8e7ba746f7e48c806fdae82ce472dd20
Chris <chris.lucas@bris.ac.uk>
2014-08-18 15:31:15 +0100

    add 4body input files
    Former-commit-id: 0c4356d1c8d2f790398a28998b7ce47644328cd9

---
+++ sms_8/T2_4body/had/v1/make_links.sh
@@ -0,0 +1,4 @@
+ln -s T2cc_systematics.root syst.root
+ln -s sigScan_T2_4body_had_2012_100.0_bt0.0_MChi-1.0.root had.root 
+ln -s sigScan_T2_4body_had_2012_86.7_bt0.0_MChi-1.0.root had1.root
+ln -s sigScan_T2_4body_had_2012_73.7_bt0.0_MChi-1.0.root had0.root
